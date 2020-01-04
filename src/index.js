c1f7b331d138bf460d825ac553b656c001272087
Vladimir Kochenov <kochenov.vl@gmail.com>
2020-01-04 13:48:17 +0300

    Shipping Redux, Router, Devtools

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './main.css'
 
-document.write('Hello React/Redux!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
+
+import createRootReduser from 'reducers'
+import Layout from 'containers/layout'
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+
+// В createStore первым параметром передается главный reducer, остальными параметрами middlewars
+// applyMiddleware — используется, что бы применить все middlewars, применяет по очереди из массива
+const store = createStore(
+  createRootReduser(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
